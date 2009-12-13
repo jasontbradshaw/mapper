cad22ed51a0c05f69d25d88bb3b776c52f8d410c
Jason Bradshaw <jasontbradshaw@gmail.com>
2009-12-12 21:58:27 -0600

    downloading works, need to implement proxy download

--- TileDownloader.py
+++ TileDownloader.py
@@ -1,17 +1,19 @@
 from Coordinates import MercatorCoord, TileCoord
 import threading
 import urllib2
+import random
+import os
 
 from pprint import pprint
 
 """
 URL Examples
 ------------
-terrain:
-  http://mt0.google.com/vt/v=app.115&hl=en&src=api&x=164&y=394&z=10&s=Galile
-
 sattelite:
   http://khm1.google.com/kh/v=50&x=165&y=395&z=10&s=Ga
+
+terrain:
+  http://mt0.google.com/vt/v=app.115&hl=en&src=api&x=164&y=394&z=10&s=Galile
 
 overlay:
   http://mt1.google.com/vt/lyrs=h@115&hl=en&src=api&x=163&y=396&z=10&s=Galil
@@ -21,8 +23,8 @@ map:
 
 Notes:
  - 's' appears to be irrelevant, urls work without it
- - ???(0|1).google.com breaks down like so:
-     mt = terrain, overlay, and map
+ - ???[0-3].google.com breaks down like so:
+     mt = terrain, overlay, and map tiles
      khm = sattelite tiles
  - google.com/??/ tells what type of tile to get from the server, like so:
      vt = terrain, overlay, and map types
@@ -30,17 +32,77 @@ Notes:
  - google.com/xx/?=? tells what to get from the maps, overlay
      lyrs=h = overlay tiles
      lyrs=m = map tiles
+
+Sanitized (Working) Examples:
+sattelite:
+  http://khm0.google.com/kh/v=50&x=165&y=395&z=10
+
+terrain:
+  http://mt0.google.com/vt/v=app.115&x=164&y=394&z=10
+
+overlay:
+  http://mt0.google.com/vt/lyrs=h&x=163&y=396&z=10
+
+map:
+  http://mt0.google.com/vt/x=163&y=394&z=10
+
 """
 
 def main():
-    t = TileDownloader([])
+    tiles = [ TileCoord(59902, 107915, 18),
+              TileCoord(59903, 107915, 18),
+              TileCoord(59904, 107915, 18),
+              TileCoord(59905, 107915, 18),
+              TileCoord(59906, 107915, 18) ]
+    
+    t = TileDownloader("t", tiles)
+    t.download()
 
 class TileDownloader(object):
     """Downloads map tiles using multiple threads"""
     
-    def __init__(self, coord_list, num_threads = 10):
+    def __init__(self, tile_type, tile_list, num_threads = 10):
         # get the proxies we'll use to prevent Google from banning us ;)
-        self._proxy_list = self.get_proxy_list()
+        self._type = tile_type
+        
+        self._split_lists = self.split_list( tile_list, num_threads )
+        
+    def download(self):
+        """Manages the thread pool that downloads the tiles"""
+        
+        # assign threads their respective tile lists
+        thread_pool = []
+        for lst in self._split_lists:
+            thread_pool.append( DownloadThread(lst, self.get_proxy_list()) )
+        
+        for thread in thread_pool:
+            thread.run()
+        
+    def split_list(self, lst, n):
+        """Splits a list into roughly equal parts"""
+        
+        # ensure we don't split into more parts than we can have
+        n = min( len(lst), n )
+        
+        part_size = len(lst) / n
+        
+        # split the list and store it in the result list
+        split_lists = []
+        for i in xrange(n):
+            # as long as this isn't the last section of the list
+            if i < n - 1:
+                split_lists.append( lst[0:part_size] )
+                
+                # trim lst down by the amount we just split it by
+                lst = lst[part_size:]
+            else:
+                split_lists.append( lst[0:] ) # append the remaining piece
+
+        
+        for l in split_lists:
+            print len(l)
+            
+        return split_lists
     
     def get_proxy_list(self):
         # the site we'll get our list from
@@ -54,7 +116,7 @@ class TileDownloader(object):
         agent += "Chrome/4.0.249.30 Safari/532.5"
         request = urllib2.Request(url, headers = {"User-Agent": agent})
         
-        # open it and read the site's text contents
+        # open it and read the site's HTML contents
         proxy_text = urllib2.urlopen(request).read()
         
         # format the text by splitting out the thing after '<pre>' in the
@@ -62,7 +124,7 @@ class TileDownloader(object):
         proxy_text = proxy_text.split("<pre>")[1].split("</pre>")[0]
         proxy_text = proxy_text.strip()
         
-        # form a list from the lines
+        # form a list from the lines we just got
         proxy_list = proxy_text.split("\n")
         
         # format each line to the 'ProxyHandler' specification:
@@ -78,12 +140,93 @@ class TileDownloader(object):
 class DownloadThread(threading.Thread):
     """Downloads given tiles using a proxy list"""
     
-    def __init__(self, coord_list, proxy_list):
+    def __init__( self, tile_list, proxy_list, tile_type = "m",
+                  destination_directory = "" ):
         
+        self._tile_list = tile_list
+        self._proxy_list = proxy_list
+        self._dir = destination_directory
+        
+        # used by function 'generate_url'
+        self._type = tile_type
+        
+        # needs to be called by convention
         threading.Thread.__init__(self)
     
     def run(self):
-        pass
+        """Downloads the given tiles"""
+        
+        # create the download directory, if it doesn't exist
+        try:
+            os.mkdir( os.path.join( str(self._dir), str(self._type) ) )
+        except OSError, e:
+            # ignore preexisting directory, since we probably created it
+            pass
+        
+        # spoof the user agent again (just in case this time)
+        agent  = "Mozilla/5.0 (X11; U; Linux x86_64; en-US) "
+        agent += "AppleWebKit/532.5 (KHTML, like Gecko) "
+        agent += "Chrome/4.0.249.30 Safari/532.5"
+        
+        # download every TileCoord this thread was given
+        for tile in self._tile_list:
+            # build the url we'll use to download this tile
+            url = self.generate_url( tile )
+            
+            request = urllib2.Request(url, headers = {"User-Agent": agent})
+            
+            # save the tile to a file (in a style, by the while...)
+            fname = os.path.join( str( self._dir ),
+                                  str( self._type ),
+                                  ( str( tile.get_x() ) + "-" +
+                                    str( tile.get_y() ) + "-" +
+                                    str( tile.get_zoom() ) ) )
+            
+            with open(fname, "w") as tfile:
+                # download, read, then write the tile data
+                tfile.write( urllib2.urlopen(request).read() )
+                
+    def generate_url(self, tile_coord):
+        """Generates a new download url based on the tile_type given."""
+        
+        url = "http://"
+        
+        # select the approproate server name (map, terrain, or overlay)
+        if self._type == "m" or self._type == "t" or self._type == "o":
+            url += "mt"
+        else: # satellite
+            url += "khm"
+        
+        # choose a random server number
+        url += str( random.randint(0, 3) )
+        url += ".google.com/"
+        
+        # select the correct identifier
+        if self._type == "m" or self._type == "t" or self._type == "o":
+            url += "vt"
+        else:
+            url += "kh"
+        
+        url += "/"
+        
+        # specify type of tiles ('m' needs no special parameters)
+        if self._type == "t":
+            url += "v=app.115&"
+        
+        if self._type == "o":
+            url += "lyrs=h&"
+        
+        if self._type == "s":
+            url += "v=50&"
+        
+        # insert coordinates and zoom from the given TileCoord
+        url += "x=" + str( tile_coord.get_x() )
+        url += "&"
+        url += "y=" + str( tile_coord.get_y() )
+        url += "&"
+        url += "z=" + str( tile_coord.get_zoom() )
+        
+        return url
 
 if __name__ == "__main__":
     main()
