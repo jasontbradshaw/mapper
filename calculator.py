7629f991e35a6f86adf2def84e1bf0168ca25239
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-12-29 00:51:10 -0600

    Add get_area method to TileDownloader

--- calculator.py
+++ calculator.py
@@ -10,6 +10,61 @@ class TileCalculator:
     def __init__(self):
         raise NotImplemented("Can't instantiate " + self.__class__.__name__)
 
+    def get_area(merc_coords, zoom):
+        """
+        Gets all the tiles in an area for some zoom level and returns them as a
+        set. Coordinates are assumed to be in-order, and the polygon they
+        describe to be non-complex (have no overlapping edges). Coordinates are
+        constrained to the given zoom.
+        """
+
+        # don't do calculations if we we'rent given any vertices
+        if len(merc_coords) == 0:
+            return set()
+
+        # make sure all the coords have the same zoom level
+        tile_vertices = []
+        for m in merc_coords:
+            # create a copy with the required zoom, then convert to a tile coord
+            m_copy = MercatorCoord(m.latitude, m.longitude, zoom)
+            tile_vertices.append(m_copy.convert())
+
+        # initialize the set with our initial vertices
+        result = set(tile_vertices)
+
+        # find the top, bottom, and left-most vertices (y increases down, x
+        # increases right).
+        top = None
+        bottom = None
+        left = None
+        for vertex in result:
+            # left
+            if vertex.x < left.x:
+                left = vertex
+
+            # top
+            if vertex.y < top.y:
+                top = vertex
+
+            # bottom
+            if vertex.y > bottom.y:
+                bottom = vertex
+
+        # add lines between consecutive vertices
+        prev_vertex = tile_vertices[0]
+        for vertex in tile_vertices[1:]:
+            result.update(TileCalculator.get_line(
+                prev_vertex.convert(), vertex.convert(), zoom))
+            prev_vertex = vertex
+
+        # cast rays left to right, top to bottom, adding tiles that lie within
+        # the polygon (calculated using edge intersection counts).
+        for y in xrange(top.y, bottom.y):
+            # TODO: handle corners!
+            pass
+
+        return result
+
     @staticmethod
     def get_line(merc0, merc1, zoom):
         """
