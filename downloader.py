0dae3db3cd19686e5e3942ee27e01e086f63b475
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2011-12-28 18:18:49 -0600

    Complete refactor of TileDownloader

--- downloader.py
+++ downloader.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-import urllib2
-from urllib2 import HTTPError
-
-import threading
-import random
 import os
+import Queue
+import random
+import threading
+import urllib2
 
 from coordinates import MercatorCoord, TileCoord
 
@@ -17,140 +16,108 @@ class TileDownloader:
     TILE_TYPE_TERRAIN = "terrain"
     TILE_TYPE_SATELLITE = "satellite"
 
-    def __init__(self, tile_type, tile_list, num_threads=4):
-        self.tile_type = tile_type
-        self.tile_list = tile_list
-        self.num_threads = num_threads
+    def __init__(self, tiles):
+        raise NotImplemented("Can't instantiate " + self.__class__.__name__)
 
-    def download(self):
-        """Manages the thread 'pool' that downloads the tiles."""
+    @staticmethod
+    def download(tile_type, tiles, num_threads=4):
+        """Downloads some tiles using the given type."""
+
+        # downloads the tiles in the given queue until no tiles remain
+        def download_tiles(tile_type, tile_queue):
+            while 1:
+                try:
+                    tile = tile_queue.get_nowait()
+                    TileDownloader.download_tile(tile_type, tile)
+                except Queue.Empty:
+                    break
+
+        # put all our tiles into a queue so all threads can share them
+        tile_queue = Queue.Queue()
+        [tile_queue.put(tile) for tile in tiles]
 
         # assign threads their respective tile lists
         thread_pool = []
-        for tile_list in self.split_list(self.tile_list, self.num_threads):
-            thread_pool.append(DownloadThread(tile_list, self.tile_type) )
-
-        # start all the threads we just created
-        for thread in thread_pool:
+        for i in xrange(num_threads):
+            thread = threading.Thread(target=download_tiles,
+                    args=[tile_type, tile_queue])
+            thread_pool.append(thread)
             thread.start()
 
         # wait for all the threads to finish
-        for thread in thread_pool:
-            thread.join()
+        [thread.join() for thread in thread_pool]
 
-    def split_list(self, lst, n):
-        """Splits a list into roughly n equal parts."""
-
-        # ensure we don't split into more parts than we have
-        n = min(len(lst), n)
-
-        # round-robin the items into the list of lists (ie. 'split_lists')
-        counter = 0
-
-        # can't use '[[]] * n' because that simply copies a reference to the
-        # same empty list into split_lists n times
-        split_lists = []
-        for i in xrange(n):
-            split_lists.append([])
-
-        for item in lst:
-            # wrap counter to distribute items evenly
-            if counter >= n:
-                counter = 0
-
-            # add this item to one of the bins in split_lists
-            split_lists[counter].append(item)
-
-            counter += 1
-
-        return split_lists
-
-class DownloadThread(threading.Thread):
-    """Downloads given tiles."""
-
-    def __init__(self, tile_list, tile_type = "m", destination_directory=""):
-
-        self._tile_list = tile_list
-        self._dir = destination_directory
-
-        # used by function 'generate_request'
-        self._type = tile_type
-
-        # needs to be called by convention
-        threading.Thread.__init__(self)
-
-    def run(self):
-        """Downloads the given tiles"""
+    @staticmethod
+    def download_tile(tile_type, tile):
+        """Downloads a single tile with the given type."""
 
         # create the download directory, if it doesn't exist
         try:
-            os.mkdir(os.path.join(str(self._dir), str(self._type)))
+            os.mkdir(tile_type)
         except OSError, e:
-            # ignore preexisting directory, since we probably created it
-            pass
+            # ignore 'directory already exists' errors, propogate all others
+            if e.errno != 17:
+                raise e
 
-        # download every TileCoord this thread was given
-        for tile in self._tile_list:
-            # fill in a random server number [0-3]
-            url = "http://mt%d.google.com/vt/v=" % random.randint(0, 3)
+        # fill in a random server number [0-3]
+        url = "http://mt%d.google.com/vt/v=" % random.randint(0, 3)
 
-            # specify type of tiles we want
-            # map
-            if self._type == TileDownloader.TILE_TYPE_MAP:
-                url += "m"
+        # specify type of tiles we want
+        # map
+        if tile_type == TileDownloader.TILE_TYPE_MAP:
+            url += "m"
 
-            # terrain
-            # TODO: make terrain downloading work at all
-            elif self._type == TileDownloader.TILE_TYPE_TERRAIN:
-                url += "p"
+        # terrain
+        # TODO: make terrain downloading work at all
+        elif tile_type == TileDownloader.TILE_TYPE_TERRAIN:
+            url += "p"
 
-            # overlay
-            elif self._type == TileDownloader.TILE_TYPE_OVERLAY:
-                url += "h"
+        # overlay
+        elif tile_type == TileDownloader.TILE_TYPE_OVERLAY:
+            url += "h"
 
-            # satellite
-            elif self._type == TileDownloader.TILE_TYPE_SATELLITE:
-                url += "y"
+        # satellite
+        elif tile_type == TileDownloader.TILE_TYPE_SATELLITE:
+            url += "y"
 
-            else:
-                # simple error report, download should fail with an HTTPError
-                print "Tile type " + self._type + "' was not recognized."
+        else:
+            # simple error report, download should fail with an HTTPError
+            print "Tile type " + tile_type + "' was not recognized."
 
-            # get ready for next parameters...
-            url += "&"
+        # get ready for next parameters...
+        url += "&"
 
-            # insert coordinates and zoom from the given TileCoord
-            url += "x=" + str(tile.x)
-            url += "&"
-            url += "y=" + str(tile.y)
-            url += "&"
-            url += "z=" + str(tile.zoom)
+        # insert coordinates and zoom from the given TileCoord
+        url += "x=" + str(tile.x)
+        url += "&"
+        url += "y=" + str(tile.y)
+        url += "&"
+        url += "z=" + str(tile.zoom)
 
-            # spoof the user agent again (just in case this time)
-            agent  = "Mozilla/5.0 (X11; U; Linux x86_64; en-US) "
-            agent += "AppleWebKit/532.5 (KHTML, like Gecko) "
-            agent += "Chrome/4.0.249.30 Safari/532.5"
+        # spoof the user agent again (just in case this time)
+        agent  = "Mozilla/5.0 (X11; U; Linux x86_64; en-US) "
+        agent += "AppleWebKit/532.5 (KHTML, like Gecko) "
+        agent += "Chrome/4.0.249.30 Safari/532.5"
 
-            request = urllib2.Request(url, headers={"User-Agent": agent})
+        request = urllib2.Request(url, headers={"User-Agent": agent})
 
-            # save the tile to a file (in a style, by the while...).
-            # overwrites previous content without asking
-            fname = os.path.join(str(self._dir),
-                                 str(self._type),
-                                 (str(tile.x) + "-" +
-                                  str(tile.y) + "-" +
-                                  str(tile.zoom)))
+        # save the tile to a file (in a style, by the while...).
+        # overwrites previous content without asking
+        fname = os.path.join(str(tile_type),
+                             (str(tile.x) + "-" +
+                              str(tile.y) + "-" +
+                              str(tile.zoom)))
 
-            # download and read the tile data
-            try:
-                tile_data = urllib2.urlopen(request).read()
-            except HTTPError, e:
-                print "Failed to download '" + str(tile) + "', aborting."
-                break
+        try:
+            # download and save the tile data
+            tile_data = urllib2.urlopen(request).read()
 
             # write the tile to its file
             with open(fname, "w") as tfile:
                 tfile.write(tile_data)
+        except urllib2.HTTPError, e:
+            print "Failed to download '" + str(tile) + "', aborting."
+            raise e
 
 if __name__ == "__main__":
     # these tiles represent roughly the UT Austin campus
@@ -186,5 +153,4 @@ if __name__ == "__main__":
         TileCoord(59906, 107919, 18)
     ]
 
-    t = TileDownloader(TileDownloader.TILE_TYPE_MAP, tiles)
-    t.download()
+    TileDownloader.download(TileDownloader.TILE_TYPE_MAP, tiles)
