f12a45fc4fc290632624d9e0987f18a6b7372368
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2012-01-09 23:01:54 -0600

    Simplify variable assignment

--- mapper.py
+++ mapper.py
@@ -479,10 +479,8 @@ class Polygon:
         """
 
         # make some shorthand variables
-        x0 = a[0]
-        y0 = a[1]
-        x1 = b[0]
-        y1 = b[1]
+        x0, y0 = a
+        x1, y1 = b
 
         # an optimized version of the algorithm (see #Simplification in wiki)
         dx = abs(x1 - x0)
