a327475ec47c98e5fab55227efae0c4e77f311c6
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2012-01-14 12:46:03 -0600

    Add right-click menu, polygon exporting
    Right-clicking a polygon now offers two options: delete and export. Delete
    replaces the old clicking method, and export offers a file download of tuples of
    polygon coordinates.

--- area_selector/js/area_selector.js
+++ area_selector/js/area_selector.js
@@ -2,23 +2,51 @@ $(function () {
     // force less.js to stop caching files while developing!
     destroyLessCache("css");
 
-    // keep track of currently active polygons
-    var polygons = [];
+    // keep track of keyboard and mouse state
+    var keypressTracker = setupKeypressTracker();
+    var mouseTracker = setupMouseTracker();
 
-    // keep track of currently pressed keys
-    var pressedKeys = setupKeypressTracker();
+    setupMenu();
 
     // build the main map
     var map = setupMap();
 
     // allow creation of polygons
-    setupPolygon(map, polygons, pressedKeys);
+    setupPolygon(map, keypressTracker, mouseTracker);
 });
+
+// create and return an object that always holds the current mouse coordinates
+var setupMouseTracker = function () {
+    // an object for tracking mouse location
+    var MouseTracker = function () {
+        this.__x = 0;
+        this.__y = 0;
+
+        // returns the last known mouse position
+        this.getMousePos = function () {
+            return [this.__x, this.__y];
+        };
+
+        this.__setMousePos = function (x, y) {
+            this.__x = x;
+            this.__y = y;
+        };
+    };
+
+    var mouseTracker = new MouseTracker();
+
+    // modify it every time the mouse moves
+    $(window).mousemove(function (e) {
+        mouseTracker.__setMousePos(e.pageX, e.pageY);
+    });
+
+    return mouseTracker;
+};
 
 // creates and returns an object that tracks currently pressed keyboard keys
 var setupKeypressTracker = function () {
     // an object for tracking pressed keys
-    var PressedKeys = function () {
+    var KeypressTracker = function () {
         this.__map = {};
 
         this.__addPressed = function (keyCode) {
@@ -36,14 +64,29 @@ var setupKeypressTracker = function () {
     };
 
     // create our own instance of the keypress tracker
-    var pressedKeys = new PressedKeys();
+    var keypressTracker = new KeypressTracker();
 
     // update it when keys are pressed/released
-    $(window).keydown(function (e) { pressedKeys.__addPressed(e.which); });
-    $(window).keyup(function (e) { pressedKeys.__removePressed(e.which); });
+    $(window).keydown(function (e) { keypressTracker.__addPressed(e.which); });
+    $(window).keyup(function (e) { keypressTracker.__removePressed(e.which); });
 
-    return pressedKeys;
+    return keypressTracker;
 };
+
+// make the menu hide when it is clicked on, or off, of
+var setupMenu = function () {
+    var menu = $("#menu");
+    var body = $("body");
+
+    var menuHide = function () {
+        menu.hide();
+    };
+
+    // hide the menu after it or the body is clicked
+    body.click(menuHide);
+    menu.click(menuHide);
+
+}
 
 // initialize the main map
 var setupMap = function () {
@@ -65,11 +108,10 @@ var setupMap = function () {
     return map;
 };
 
-// make clicking create a polygon on the map
-var setupPolygon = function (map, polygons, pressedKeys) {
+// make clicking create a polygon on the map, and right-clicking show its menu
+var setupPolygon = function (map, keypressTracker, mouseTracker) {
     // keys for collection points and deleting polygons
     var collectorKeyCode = 16; // shift
-    var deleteKeyCode = 17; // control
 
     // collect coordinates as a line that we can convert to a polygon later
     var polyline = null;
@@ -87,11 +129,9 @@ var setupPolygon = function (map, polygons, pressedKeys) {
                     editable: true,
                 });
 
-                // make clicking the polygon with the delete key held remove it
-                google.maps.event.addListener(polygon, "click", function (e) {
-                    if (pressedKeys.isPressed(deleteKeyCode)) {
-                        polygon.setMap(null);
-                    }
+                // make right-clicking the polygon show the menu for it
+                google.maps.event.addListener(polygon, "rightclick", function (e) {
+                    showMenu(polygon, mouseTracker);
                 });
             }
 
@@ -103,7 +143,7 @@ var setupPolygon = function (map, polygons, pressedKeys) {
 
     // build the line if the collect key is held down
     google.maps.event.addListener(map, "click", function (e) {
-        if (pressedKeys.isPressed(collectorKeyCode)) {
+        if (keypressTracker.isPressed(collectorKeyCode)) {
             // create the polyline if it doesn't exist yet
             if (polyline === null) {
                 polyline = new google.maps.Polyline({
@@ -116,6 +156,66 @@ var setupPolygon = function (map, polygons, pressedKeys) {
             polyline.getPath().push(e.latLng);
         }
     });
+};
+
+// shows the menu at the mouse location, and makes the options reference the
+// given polygon.
+var showMenu = function (selectedPolygon, mouseTracker) {
+    var menu = $("#menu");
+    var deleteItem = $(".menu_item.delete");
+    var exportItem = $(".menu_item.export");
+
+    // remove old bindings for the menu items
+    deleteItem.unbind("click");
+    exportItem.unbind("click");
+
+    // make clicking 'delete' remove the given polygon
+    deleteItem.click(function () {
+        selectedPolygon.setMap(null);
+    });
+
+    // make clicking 'export' offer the current polygon for download
+    exportItem.click(function () {
+        exportPolygon(selectedPolygon);
+    });
+
+    // move the menu to the current mouse location and show it
+    var mousePos = mouseTracker.getMousePos();
+    menu.css("left", mousePos[0]);
+    menu.css("top", mousePos[1]);
+    menu.show();
+};
+
+// offer up the given polygon for download
+var exportPolygon = function (polygon) {
+    console.log("Exporting polygon...");
+    var polyString = polygonToString(polygon);
+
+    // create a data URI and put the encoded data in it. we use the MIME type of
+    // application/octet-stream to force a file download.
+    var uriContent = "data:application/octet-stream;charset=utf-8,";
+    uriContent += encodeURIComponent(polyString);
+
+    // tell the window to offer a download of the polygon data
+    location.href = uriContent;
+};
+
+// collect the latitude/longitude vertices in a polygon as a string with the
+// format: (lat0, lng0)\n(lat1, lng1)\n...(latN, lngN)\n
+var polygonToString = function (polygon) {
+    var stringData = "";
+    var addCoord = function (coord) {
+        // create line-separated tuples of (latitude, longitude)
+        stringData += "(" + coord.lat() + ", " + coord.lng() + ")";
+        stringData += "\n";
+    };
+
+    // gather the polygon's coordinates into a string
+    polygon.getPath().forEach(function (coord) {
+        addCoord(coord);
+    });
+
+    return stringData;
 };
 
 // destroys the localStorage copy of CSS that less.js creates
