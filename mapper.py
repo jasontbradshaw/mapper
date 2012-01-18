d023d9a7268c3ef917dd88dad40354a3a085ea4e
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2012-01-17 20:39:58 -0600

    Fix TileDownloadError creation

--- mapper.py
+++ mapper.py
@@ -425,9 +425,9 @@ class Tile:
 
         # pass exceptions along for the caller to handle
         except urllib2.URLError, e:
-            raise TileDownloadError(e.message)
+            raise Tile.TileDownloadError(e.message)
         except urllib2.HTTPError, e:
-            raise TileDownloadError(e.message)
+            raise Tile.TileDownloadError(e.message)
 
     def hash_google(self):
         """
